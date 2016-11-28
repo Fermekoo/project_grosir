aef38b4a671a0423f3e66f6a613d198c01a00f1d
Fermekoo <dandifermeko@gmail.com>
2016-11-28 23:06:50 +0700

    ubah penambahan stok toko

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $db_host = "localhost";
 $db_user = "root";
-$db_pass = "talingok";
+$db_pass = "";
 $db_name = "grosir";
 
 $koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
